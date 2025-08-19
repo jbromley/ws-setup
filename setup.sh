af904159e22f7e682a0841af7f85894214006873
J. Bromley <jay@appliedinvention.com>
2025-08-19 13:59:12 -0700

    Initial commit
    First version of workspace setup script.

---
+++ setup.sh
@@ -0,0 +1,229 @@
+#!/usr/bin/env bash
+# Template from https://betterdev.blog/minimal-safe-bash-script-template/
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+usage() {
+  cat << EOF # remove the space between << and EOF, this is due to web plugin issue
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-f] -p param_value arg1 [arg2...]
+
+Script description here.
+
+Available options:
+
+-h, --help      Print this help and exit
+-v, --verbose   Print script debug info
+EOF
+  exit
+}
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    # shellcheck disable=SC2034
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+parse_params() {
+  # default values of variables set from params
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose) set -x ;;
+    --no-color) NO_COLOR=1 ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  return 0
+}
+
+parse_params "$@"
+setup_colors
+
+# The script will need sudo, get authorization now.
+sudo -k
+if ! sudo -v; then
+  echo "sudo authentication required." >&2
+  exit 1
+fi
+
+# Keep the sudo timestamp fresh until this script exits.
+# shellcheck disable=SC2064
+trap 'kill "${SUDO_KEEPALIVE_PID:-}" 2>/dev/null || true; sudo -k || true' EXIT
+( while true; do sudo -n true; sleep 60; done ) &
+SUDO_KEEPALIVE_PID=$!
+
+function needroot() { sudo -n "$@"; }
+
+# script logic here
+
+function install_dotfiles {
+  local repo="$1"
+  local dotfiles_dir="$2"
+  local dotfiles="aliases config/{atuin,bat,emacs,helix,kitty,nvim,tmux,yazi} dircolors gitignore rcrc sbcl{_completions,rc} zsh zshenv zshrc"
+
+  msg "${GREEN}Installing ${repo} to ${dotfiles_dir}${NOFORMAT}"
+  needroot apt install --yes git rcm
+  git clone "${repo}" "${dotfiles_dir}"
+  cd "${dotfiles_dir}"
+  for dotfile in ${dotfiles}; do
+    rcup -v "${dotfile}"
+  done
+
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions ~/.zsh
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh
+  git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh
+}
+
+function install_deb {
+  local url="$1"
+  local name
+  name=$(basename "${url}")
+
+  msg "${GREEN}Installing ${name} from ${url}${NOFORMAT}"
+  curl --silent --show-error --remote-name "${url}"
+  needroot dpkg --install "${name}"
+  rm "${name}"
+}
+
+function install_archive {
+  local executable="$1"
+  local url="$2"
+  local name
+  name=$(basename "${url}")
+
+  msg "${GREEN}Installing ${executable} from ${url}${NOFORMAT}"
+  curl --silent --show-error --remote-name "${url}"
+  tar -xf "${name}" "${executable}"
+  mv "${executable}" ~/.local/bin
+  rm "${name}"
+}
+
+function install_yazi {
+  local url="$1"
+  local name
+  local zipdir
+  name=$(basename "${url}")
+  zipdir=${name%.*}
+
+  msg "${GREEN}Installing yazi from ${url}${NOFORMAT}"
+  curl --silent --show-error --remote-name "${url}"
+  unzip -qq "${name}"
+  mv "${zipdir}/{ya,yazi}" ~/.local/bin/
+  mv "${zipdir}/completions/{_ya,_yazi}" ~/.zsh/
+  rm -rf "${zipdir}"
+}
+
+function install_kitty {
+  msg "${GREEN}Installing kitty${NOFORMAT}"
+  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+  mkdir -p ~/.local/share/applications
+  ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+  cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+  cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+  sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+  sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+  echo 'kitty.desktop' > ~/.config/xdg-terminals.list
+
+  mkdir -p ~/.terminfo/x
+  cp ~/.local/kitty.app/share/terminfo/x/xterm-kitty ~/.terminfo/x/
+}
+
+function install_atuin {
+  msg "${GREEN}Installing atuin${NOFORMAT}"
+  bash <(curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh)
+}
+
+function install_mise {
+  local mise="${HOME}/.local/bin/mise"
+  local runtimes
+  runtimes=$(cat ./runtimes)
+
+  msg "${GREEN}Installing mise${NOFORMAT}"
+
+  curl https://mise.run | sh
+
+  for runtime in ${runtimes}; do
+    plugin=${runtime//@[0-9\.]*/}
+    # shellcheck disable=SC2076
+    if [[ ! " node rust " =~ " ${plugin} " ]]; then
+      ${mise} plugin install "${plugin}"
+    fi
+    ${mise} use --global "${runtime}"
+  done
+}
+
+function install_packages {
+  msg "${GREEN}Installing Ubuntu packages${NOFORMAT}"
+  needroot xargs apt install --yes < ./packages
+
+  # Docker
+  for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
+    needroot apt remove $pkg
+  done
+
+  needroot install -m 0755 -d /etc/apt/keyrings
+  needroot curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  needroot chmod a+r /etc/apt/keyrings/docker.asc
+
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    needroot tee /etc/apt/sources.list.d/docker.list > /dev/null
+  needroot apt-get update
+
+  needroot apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+  # Kicad
+  needroot add-apt-repository ppa:kicad/kicad-9.0-releases
+  needroot apt update
+  needroot apt install kicad
+}
+
+function configure_user {
+  local user="$1"
+
+  needroot usermod --append --groups kvm,tcpdump,wireshark,libvirt,docker
+  chsh -s /usr/bin/zsh "${user}"
+}
+
+# Main entry point
+needroot apt update
+needroot apt upgrade --yes
+mkdir ~/.local/bin
+
+install_dotfiles git@github.com:jbromley/dotfiles.git ~/.dotfiles
+install_deb https://github.com/sharkdp/bat/releases/download/v0.25.0/bat_0.25.0_amd64.deb
+install_deb https://github.com/ajeetdsouza/zoxide/releases/download/v0.9.8/zoxide_0.9.8-1_amd64.deb
+install_deb https://github.com/helix-editor/helix/releases/download/25.07.1/helix_25.7.1-1_amd64.deb
+install_archive starship https://github.com/starship/starship/releases/download/v1.23.0/starship-x86_64-unknown-linux-gnu.tar.gz
+install_archive lazygit https://github.com/jesseduffield/lazygit/releases/download/v0.54.2/lazygit_0.54.2_linux_x86_64.tar.gz
+install_archive fzf https://github.com/junegunn/fzf/releases/download/v0.65.1/fzf-0.65.1-linux_amd64.tar.gz
+install_yazi https://github.com/sxyazi/yazi/releases/download/v25.5.31/yazi-x86_64-unknown-linux-gnu.zip
+install_kitty
+install_atuin
+install_mise
+install_packages
+configure_user jay
